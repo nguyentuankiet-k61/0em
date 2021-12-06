6d624b94a2afccea2bf6d02eae899cbf284c35a1
nguyentuankiet-k61 <72546259+nguyentuankiet-k61@users.noreply.github.com>
2021-12-06 16:19:40 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-const nameGirl = 'Em';
+const nameGirl = 'Mai Lê Ái Linh';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Mai Lê Aí Linh';
+const contentCard = ' Gửi đến Mai Lê Aí Linh , cô gái năm 20 tuổi. Chúc mừng cô gái tròn 20 tuổi! Chúc cô gái trong hình tuổi mới luôn hạnh phúc và tràn ngập tiếng cười nhé.
+ Chính thức bước sang tuổi 20 độ tuổi chênh vênh nhất của cuộc đời, phải tự định hướng cho mình một tương lai. Đừng mong đợi bất cứ điều gì từ người khác mà hãy tự mình thực hiện những điều mình muốn. Đừng đặt hy vọng vào ai quá lớn để khỏi phải thất vọng ê chề. Đừng tin những lời người khác nói mà hãy nhìn vào những gì họ làm được… Hãy tập sống “Để gió cuốn đi”. Luôn trân trọng những gì mình đang có nhé cô gái… Chúc cô gái sinh nhật vui vẻ.😘🥰💘';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
