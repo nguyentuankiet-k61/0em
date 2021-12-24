6aafd3ce37fcaf835f1ecbe51e78c12701ada616
nguyentuankiet-k61 <72546259+nguyentuankiet-k61@users.noreply.github.com>
2021-12-24 22:03:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const nameGirl = 'Mai Lê Ái Linh';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng Sinh Nhật';
+const eventName = ' Merry Christmas';
 const titleCard = 'Tặng Mai Lê ÁI Linh';
-const contentCard = ' khỏ gió cuốn đi”. Luôn trân trọng những gì mình đang có nhé cô gái… Chúc cô gái sinh nhật vui vẻ.😘🥰💘';
+const contentCard = ' Giáng sinh nữa lại về. Chúc cho ai đó hạnh phúc bên cạnh nửa yêu yêu thương và chúc ai đó đang cô đơn sẽ tìm thấy được bờ vai, cái nắm tay ấm áp. Chúc cho ai đó tìm được nhau sau tháng ngày xa cách.Chúc cho mọi người mùa Giáng sinh an lành, mùa đông ấm áp bên cạnh gia đình, người thân yêu. Merry Christmas.😘🥰💘';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
